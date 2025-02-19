a67252a9989901acc6cc34c76c860fd175ac22ee
rbiancosilva <rbiancosilva45@gmail.com>
2025-02-19 15:38:44 -0300

    Adding viewsets

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'order',
-    'product'
+    'product',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
--- product/serializers/product_serializer.py
+++ product/serializers/product_serializer.py
@@ -1,8 +1,41 @@
 from rest_framework import serializers
-from product.models.product import Product
+from product.models.product import Product, Category
 from product.serializers.category_serializer import CategorySerializer
 class ProductSerializer(serializers.ModelSerializer):
-    category = CategorySerializer(many=True, required=True)
+    category = CategorySerializer(many=True)
     class Meta:
         model = Product
         fields = ["title", "description", "price", "active", "category"]
+
+    def create(self, validated_data):
+        # Extract the nested 'category' data
+        categories_data = validated_data.pop('category')
+        
+        # Create the Product instance
+        product = Product.objects.create(**validated_data)
+        
+        # Create or update the nested Category instances
+        for category_data in categories_data:
+            category, created = Category.objects.get_or_create(**category_data)
+            product.category.add(category)
+        
+        return product
+
+    def update(self, instance, validated_data):
+        # Extract the nested 'category' data
+        categories_data = validated_data.pop('category')
+        
+        # Update the Product instance
+        instance.title = validated_data.get('title', instance.title)
+        instance.description = validated_data.get('description', instance.description)
+        instance.price = validated_data.get('price', instance.price)
+        instance.active = validated_data.get('active', instance.active)
+        instance.save()
+        
+        # Clear existing categories and add the new ones
+        instance.category.clear()
+        for category_data in categories_data:
+            category, created = Category.objects.get_or_create(**category_data)
+            instance.category.add(category)
+        
+        return instance
